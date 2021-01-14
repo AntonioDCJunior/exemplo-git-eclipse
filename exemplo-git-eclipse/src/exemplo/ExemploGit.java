fede8663729a75f6d8456392b7c56c3042e9455b
Antonio Júnior <antoniodiascjr@gmail.com>
2021-01-14 17:39:20 -0300

    teste de clonagem2

--- exemplo-git-eclipse/src/exemplo/ExemploGit.java
+++ exemplo-git-eclipse/src/exemplo/ExemploGit.java
@@ -4,6 +4,7 @@ public class ExemploGit {
 
 	public static void main(String[] args) {
 		System.out.println("Olá Git");
+		System.out.println("Teste de clonagem");
 		//alterado
 		//alterado do eclipse
 		//teste
